1799d0b6575cb3b4ab213f3f71ed236ed3bde39c
Anna Jang <anna.jangg@gmail.com>
2021-09-03 17:48:35 -0400

    learned if statements

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -2,7 +2,7 @@
 
 SECRET: int = 3
 
-print ("I'm thinking of a number between 1-5, what is it?")
+print("I'm thinking of a number between 1-5, what is it?")
 guess: int = int(input("What is your guess? "))
 
 if guess == SECRET:
