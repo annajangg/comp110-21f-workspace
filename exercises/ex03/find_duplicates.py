9191ce92033e9f6f77836d2f3372bc8aff4afe22
Anna Jang <anna.jangg@gmail.com>
2021-09-13 23:17:05 -0400

    completed ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,20 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730411065"
+
+word: str = input("Enter a word: ")
+i: int = 0
+dup: bool = False
+
+while i < len(word):
+    char: str = word[i]
+    j: int = i + 1
+    while j < len(word):
+        if word[j] == char:
+            dup: bool = True
+        else:
+            dup = dup
+        j = j + 1
+    i = i + 1
+
+print("Found duplicate: " + str(dup))
