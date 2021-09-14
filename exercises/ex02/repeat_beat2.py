d0cea072d2490324119af6b7f15ba6dc0b8504ee
Anna Jang <anna.jangg@gmail.com>
2021-09-14 18:31:49 -0400

    not taught

---
+++ exercises/ex02/repeat_beat2.py
@@ -0,0 +1,13 @@
+"""Repeating a beat in a loop."""
+
+__author__ = "730411065"
+
+
+# Begin your solution here...
+beat: str = str(input("What beat do you want to repeat? "))
+rep: int = int(input("How many times do you want to repeat it? "))
+
+if rep > 0:
+    print((beat + " ") * (rep - 1) + beat)
+else:
+    print("No beat...")
