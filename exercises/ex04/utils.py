0e7e2db909f0fb0b13a8d9b9f6a956fc749449be
Anna Jang <anna.jangg@gmail.com>
2021-09-30 00:22:47 -0400

    got it but need more help to understand

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,41 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730411065"
 
 
 # TODO: Implement your functions here.
+def all(xs: list[int], x: int) -> bool:
+    """Find if all ints in the list the same as given int."""
+    i: int = 0
+    if len(xs) == 0:
+        return False
+    while i < len(xs):
+        if xs[i] != x:
+            return False
+        i += 1
+    return True
+
+
+def is_equal(first: list[int], second: list[int]) -> bool:
+    """Find if every element at every index is equal in both lists."""
+    i: int = 0
+    if len(first) != len(second):
+        return False
+    while i < len(first):
+        if first[i] != second[i]:
+            return False
+        i += 1
+    return True
+
+
+def max(input: list[int]) -> int:
+    """Find the biggest int in the list."""
+    i: int = 0
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    max_num: int = input[0]
+    while i < len(input):
+        if input[i] > max_num:
+            max_num = input[i]
+        i += 1
+    return max_num
