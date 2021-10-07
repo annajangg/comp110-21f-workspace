ca63dc0ec0664f707d4b07a5a3e6f8fda73986f3
Anna Jang <anna.jangg@gmail.com>
2021-10-07 00:34:17 -0400

    omg i got it

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,46 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730411065"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns a list containing only the elements of the input list that were even."""
+    only_even_list: list[int] = []
+    i: int = 0
+    while i < len(xs):
+        if xs[i] % 2 == 0:
+            only_even_list.append(xs[i])
+        i += 1
+    return only_even_list
+
+
+def sub(a_list: list[int], start: int, end: int) -> list[int]:
+    """Returns a list which is a subset of the given list between the specificed start and end index."""
+    if start < 0:
+        start = 0
+    if end > len(a_list):
+        end = len(a_list)
+    sub_list: list[int] = []
+    if len(a_list) == 0 or start >= len(a_list) or end <= 0:
+        return sub_list
+    else:
+        while start < end:
+            sub_list.append(a_list[start])
+            start += 1
+        return sub_list
+
+
+def concat(first_list: list[int], sec_list: list[int]) -> list[int]:
+    """Returns a list that contains all the elements of the first list followed by all of the elements of the second list."""
+    comb_list: list[int] = []
+    i: int = 0
+    j: int = 0
+    while i < len(first_list):
+        comb_list.append(first_list[i])
+        i += 1
+    while j < len(sec_list):
+        comb_list.append(sec_list[j])
+        j += 1
+    return comb_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,111 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730411065"
+
+
+def test_only_evens_empty() -> None:
+    """Tests if the numbers in the list are even."""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_one() -> None:
+    """Tests if the numbers in the list are even."""
+    xs: list[int] = [1, 2, 3]
+    assert only_evens(xs) == [2]
+
+
+def test_only_evens_two() -> None:
+    """Tests if the numbers in the list are even."""
+    xs: list[int] = [1, 5, 3]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_three() -> None:
+    """Tests if the numbers in the list are even."""
+    xs: list[int] = [4, 4, 4]
+    assert only_evens(xs) == [4, 4, 4]
+
+
+def test_only_evens_negative() -> None:
+    """Tests if the numbers in the list are even."""
+    xs: list[int] = [-4, -4, -4]
+    assert only_evens(xs) == [-4, -4, -4]
+
+
+def test_sub_empty() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([], 0, 1) == []
+
+
+def test_sub_startgreater() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40], 8, 3) == []
+
+
+def test_sub_startzero() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40], 0, 3) == [10, 20, 30]
+
+
+def test_sub_zero() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40], 1, 0) == []
+
+
+def test_sub_negative() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40, 50, 60], -1, 3) == [10, 20, 30]
+
+
+def test_sub_greater() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40], 1, 5) == [20, 30, 40]
+
+
+def test_sub_works() -> None:
+    """Tests giving subsets of a list."""
+    assert sub([10, 20, 30, 40], 1, 3) == [20, 30]
+
+
+def test_concat_1empty2single() -> None:
+    """Test if the two lists are combined."""
+    assert concat([], [4]) == [4]
+
+
+def test_concat_1empty2multiple() -> None:
+    """Test if the two lists are combined."""
+    assert concat([], [4, 5, 6, 7, 8, 9]) == [4, 5, 6, 7, 8, 9]
+
+
+def test_concat_1single2empty() -> None:
+    """Test if the two lists are combined."""
+    assert concat([2], []) == [2]
+
+
+def test_concat_1multiple2empty() -> None:
+    """Test if the two lists are combined."""
+    assert concat([0, 1, 2, 3], []) == [0, 1, 2, 3]
+
+
+def test_concat_empty() -> None:
+    """Test if the two lists are combined."""
+    assert concat([], []) == []
+
+
+def test_concat_identical() -> None:
+    """Test if the two lists are combined."""
+    assert concat([1], [1]) == [1, 1]
+
+
+def test_concat_samelength() -> None:
+    """Test if the two lists are combined."""
+    assert concat([1, 2], [1, 3]) == [1, 2, 1, 3]
+
+
+def test_concat_diff() -> None:
+    """Test if the two lists are combined."""
+    assert concat([3, 5], [4, 6, 8]) == [3, 5, 4, 8]
