49edcbf95e9f1a02e63dfa58f093efa1f9d9c6b0
Anna Jang <anna.jangg@gmail.com>
2021-10-27 00:43:31 -0400

    feeling smart, thanks office hours

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,43 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730411065"
 
 # Define your functions below
+
+
+def invert(i_dict: dict[str, str]) -> dict[str, str]:
+    """Function to invert the entries in a dictionary."""
+    inverted: dict[str, str] = {}
+    for key in i_dict:
+        if i_dict[key] in inverted:
+            raise KeyError("There are more than one of the same key.")
+        inverted[i_dict[key]] = key
+    return inverted
+ 
+
+def favorite_color(i_fav: dict[str, str]) -> str:
+    """Function to give the color that is most popular."""
+    counter: dict[str, int] = {}
+    for name in i_fav:
+        if i_fav[name] in counter:
+            counter[i_fav[name]] += 1
+        else:
+            counter[i_fav[name]] = 1
+    most: int = 0
+    most_color: str = ""
+    for color in counter:
+        if counter[color] > most:
+            most = counter[color]
+            most_color = color
+    return most_color
+
+
+def count(freq: list[str]) -> dict[str, int]:
+    """Function to count the frequency the numbers come up."""
+    result = {}
+    for item in freq:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,56 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730411065"
+
+
+def test_invert_letters() -> None:
+    """Tests if the letters are inverted."""
+    assert invert({"a": "z", "b": "y", "c": "x"}) == {"z": "a", "y": "b", "x": "c"}
+
+
+def test_invert_words() -> None:
+    """Tests if the words are inverted."""
+    assert invert({"apple": "cat"}) == {"cat": "apple"}
+
+
+def test_invert_duplicate() -> None:
+    """Tests if KeyError comes up if there are duplicate values."""
+    assert invert({}) == {}
+
+
+def test_favorite_color_given() -> None:
+    """Tests if most frequent color appears."""
+    assert favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}) == "blue"
+
+
+def test_favorite_color_another() -> None:
+    """Tests if most frequent color appears."""
+    assert favorite_color({"Anna": "pink", "Lila": "blue", "Mathangi": "pink"}) == "pink"
+
+
+def test_favorite_color_tie() -> None:
+    """Tests if first most frequent color appears if there are multiple frequent colors."""
+    assert favorite_color({"Maddee": "yellow", "Alex": "blue", "Mitchell": "blue", "Addie": "yellow"}) == "yellow"
+
+
+def test_favorite_color_same() -> None:
+    """Tests if most frequent color appears if all colors are the same."""
+    assert favorite_color({"Ruth": "green", "Katherine": "green", "Kyal": "green"}) == "green"
+
+
+def test_count_first() -> None:
+    """Tests if function works."""
+    assert count(["1", "1", "1", "5", "5", "3", "1", "3", "3", "1", "4", "4", "4", "2", "2", "2", "2"]) == {"1": 5, "2": 4, "3": 3, "4": 3, "5": 2}
+
+
+def test_count_second() -> None:
+    """Tests if function works."""
+    assert count(["5", "5", "5", "1", "1", "2", "5", "2", "2", "5", "4", "4", "4", "3", "3", "3", "2"]) == {"1": 2, "2": 4, "3": 3, "4": 3, "5": 5}
+
+
+def test_count_third() -> None:
+    """Tests if function works."""
+    assert count(["1", "2", "3", "4", "5"]) == {"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}
